3d73a28af74104515e13da5f6cf5f5dbdb85e18d
PrimOliveira <145802403+PrimOliveira@users.noreply.github.com>
2024-03-30 22:34:21 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// Aplicar máscara de telefone no formulário de contato
+$(document).ready(function(){
+    $('#phone').mask('(00) 0000-0000');
+  });
+  // Efeitos JavaScript personalizados
+$(document).ready(function(){
+    // Ativar o carrossel
+    $('.carousel').carousel();
+  
+   
+  });
